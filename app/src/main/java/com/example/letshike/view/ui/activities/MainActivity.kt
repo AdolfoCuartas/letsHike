9abfe11662c59411f393f714b681872ac881ab74
Adolfo <leonmesa29@gmail.com>
2021-12-05 18:05:20 -0500

    LetsHike login and register connection with homepage

--- app/src/main/java/com/example/letshike/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/letshike/view/ui/activities/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.letshike.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.letshike.R
+import com.example.letshike.view.ui.fragments.LoginFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        }
+
 }
